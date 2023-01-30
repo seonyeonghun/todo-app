1de8ce2686a373f64e9bd2d584ee7e552b97d142
seonyeonghun <sprax@naver.com>
2023-01-30 12:39:35 +0900

    할일등록여부 체크 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import "./App.css";
 function App() {
   const [todo, setTodo] = useState([]); // todo 라는 state, setTodo라는 modifiter 함수
   const [todos, setTodos] = useState([]); //todos 라는 state(=할일모음), setTodos라는 modifier 함수
-  const onChange = (e) => setTodo(e.target.value); // 입력폼 데이터를 state에 등록
+  const onChange = (e) => {
+    setTodo(e.target.value); // 입력폼 데이터를 state에 등록
+  };
   const onSubmit = (e) => {
     e.preventDefault(); // form 새로고침 방지
+    if (todo.length === 0 || todo === "") {
+      alert("할일을 입력하세요!");
+      return false;
+    }
     setTodo(""); // 할일 입력폼 초기화
-    setTodos(todos => [...todos, todo]); // state는 새로운 값으로만 변경 가능, ES6 구조분해할당 문법
+    setTodos((todos) => [...todos, todo]); // state는 새로운 값으로만 변경 가능, ES6 구조분해할당 문법
   };
   const removeTodo = (id) => setTodos(todos.filter((todo, i) => i !== id));
   return (
@@ -32,7 +38,11 @@ function App() {
 
       <div className='output-field'>
         <ul>
-        {todos.map((todo, i) => <li key={i}>{todo} <button onClick={() => removeTodo(i)}>&times;</button></li>)}
+          {todos.map((todo, i) => (
+            <li key={i}>
+              {todo} <button onClick={() => removeTodo(i)}>&times;</button>
+            </li>
+          ))}
         </ul>
       </div>
     </div>
